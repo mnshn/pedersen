555fa457a175068ba21ffa5fa3b1cc7bbc16cb29
paulmoonshine <p.delange@uky.edu>
2021-03-12 22:49:26 +0100

    made the commit struct

--- src/protocol/commit.rs
+++ src/protocol/commit.rs
@@ -1,11 +1,32 @@
-use crate::protocol::setup::PedersenGens;
+use crate::protocol::setup::{Group, Scalar, SetupData};
+use digest::Digest;
+use rand_core::{CryptoRng, RngCore};
+use std::ops::{Add, Mul};
 
-pub struct Commitment<T> {
+pub struct Commitment<T, K> {
     commitment: T,
+    salt: K,
+    message: K,
 }
 
-impl<T> Commitment<T> {
-    fn commit(message: Vec<u8>) -> T {}
+impl<T, K> Commitment<T, K>
+where
+    T: Add<T, Output = T> + Mul<K> + Default + Group,
+    K: Default + Mul<T> + Mul<T, Output = T> + Scalar + Copy,
+{
+    fn new<R: RngCore + CryptoRng, D: Digest<OutputSize = u64>>(
+        message: Vec<u8>,
+        rng: &mut R,
+    ) -> Self {
+        let message_as_scalar: K = K::from_bytes::<D>(message);
+        let setup_data = SetupData::<T, K>::new(rng);
+        let salt = K::random(rng);
+        Commitment {
+            commitment: message_as_scalar * setup_data.g + salt * setup_data.h,
+            salt,
+            message: message_as_scalar,
+        }
+    }
 }
 
 #[cfg(test)]
--- src/protocol/setup.rs
+++ src/protocol/setup.rs
@@ -1,33 +1,34 @@
-use crate::errors::error::CommitError;
+use digest::Digest;
 use std::marker::PhantomData;
 use std::ops::{Add, Mul};
 
 use rand_core::{CryptoRng, RngCore};
 
-pub trait PedersenScalar {
+pub trait Scalar {
     fn random<R: RngCore + CryptoRng>(rng: &mut R) -> Self;
+    fn from_bytes<D: Digest<OutputSize = u64>>(bytes: Vec<u8>) -> Self;
 }
-pub trait PedersenGroup {
+pub trait Group {
     fn random<R: RngCore + CryptoRng>(rng: &mut R) -> Self;
     fn generator() -> Self;
 }
 
-pub struct PedersenGens<T, K>
+pub struct SetupData<T, K>
 where
-    T: Add<T> + Default,
+    T: Add<T, Output = T> + Default,
 {
     pub g: T,
     pub h: T,
     _marker: PhantomData<K>,
 }
 
-impl<T, K> Default for PedersenGens<T, K>
+impl<T, K> Default for SetupData<T, K>
 where
-    T: Add<T> + Default + Mul<K>,
-    K: Default + Mul<T> + Mul<T, Output = T> + PedersenScalar,
+    T: Add<T, Output = T> + Default + Mul<K>,
+    K: Default + Mul<T> + Mul<T, Output = T> + Scalar,
 {
     fn default() -> Self {
-        PedersenGens {
+        SetupData {
             g: T::default(),
             h: T::default(),
             _marker: PhantomData,
@@ -35,13 +36,13 @@ where
     }
 }
 
-impl<T, K> PedersenGens<T, K>
+impl<T, K> SetupData<T, K>
 where
-    T: Add<T> + Default + Mul<K> + PedersenGroup,
-    K: Default + Mul<T> + Mul<T, Output = T> + PedersenScalar,
+    T: Add<T, Output = T> + Default + Mul<K> + Group,
+    K: Default + Mul<T> + Mul<T, Output = T> + Scalar,
 {
-    fn new<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
-        PedersenGens {
+    pub fn new<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
+        SetupData {
             g: T::generator(),
             h: K::random(rng) * T::generator(),
             _marker: PhantomData,
@@ -52,19 +53,24 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::protocol::setup::Scalar;
     use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
     use curve25519_dalek::ristretto::RistrettoPoint;
-    use curve25519_dalek::scalar::Scalar;
+    use curve25519_dalek::scalar::Scalar as RistrettoScalar;
     use rand_core::OsRng;
+    use sha2;
 
     #[test]
     fn test_ristretto() {
-        impl PedersenScalar for Scalar {
+        impl Scalar for RistrettoScalar {
             fn random<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
-                Scalar::random(rng)
+                RistrettoScalar::random(rng)
+            }
+            fn from_bytes<D: Digest>(input: Vec<u8>) -> Self {
+                RistrettoScalar::hash_from_bytes::<sha2::Sha512>(&input)
             }
         }
-        impl PedersenGroup for RistrettoPoint {
+        impl Group for RistrettoPoint {
             fn generator() -> Self {
                 RISTRETTO_BASEPOINT_POINT
             }
@@ -73,7 +79,7 @@ mod tests {
             }
         }
         let mut rng = OsRng;
-        let test_gens: PedersenGens<RistrettoPoint, Scalar> = PedersenGens::new(&mut rng);
+        let test_gens: SetupData<RistrettoPoint, RistrettoScalar> = SetupData::new(&mut rng);
 
         assert_eq!(test_gens.g, RISTRETTO_BASEPOINT_POINT);
     }
