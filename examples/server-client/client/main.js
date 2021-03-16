50ceec26fe680d163a5f46560a50ff247444dfae
paulmoonshine <p.delange@uky.edu>
2021-03-16 17:25:51 +0100

    example client

--- examples/server-client/client/main.js
+++ examples/server-client/client/main.js
@@ -1,22 +1,33 @@
 import init, { handleMessage } from "./pkg/index.js";
 
+const makeAndSendSetupData = document.createElement('button')
+makeAndSendSetupData.id = 'button'
+makeAndSendSetupData.innerHTML = 'Generate and send setup data'
+document.body.appendChild(makeAndSendSetupData)
+
 window.addEventListener("load", async () => {
-    await init();
-    const ws = new WebSocket(`ws://${location.hostname}:8080/ws`);
-    ws.addEventListener("message", (event) => onMessage(ws, event));
-    ws.addEventListener("close", console.log("Socket closed"));
-    ws.addEventListener("error", (error) => console.log("WebSocket error", error));
-    ws.addEventListener("open", () => handleConnection(ws));
-});
+    await init()
+    const ws = new WebSocket(`ws://${location.hostname}:8080/ws`)
+    ws.addEventListener("message", (event) => onMessage(ws, event))
+    ws.addEventListener("close", console.log("Socket closed"))
+    ws.addEventListener("error", (error) => console.log("WebSocket error", error))
+    ws.addEventListener("open", () => handleConnection(ws))
+
+})
+
+const button = document.getElementById('button')
+button.addEventListener('click', () => handleClick())
 
 function handleConnection(socket) {
-    // Do something when the socket is opened
+    console.log("Connected")
 }
 
 // A handler for incoming messages from the server.
 async function onMessage(socket, event) {
-    let buffer = await event.data.arrayBuffer();
-    const data = new Uint8Array(buffer);
-    const response = handleMessage(data);
-    socket.send(response);
+    let buffer = await event.data.arrayBuffer()
+    const data = new Uint8Array(buffer)
+    const response = handleMessage(data)
+    socket.send(response)
 }
+
+const handleClick = () => console.log('click')
